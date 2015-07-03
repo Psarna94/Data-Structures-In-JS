d3b9350999c374d3a3d623456566d67da667dfbe
Pranu Sarna <pranusarna@gmail.com>
2015-07-03 22:25:16 +0530

    Corrected Directory structure

---
+++ Misc. problems/chunking an array.js
@@ -0,0 +1,19 @@
+/**
+ * Created by reliance on 02-07-2015.
+ */
+function chunk(arr, size) {
+  // Break it up.
+  var foo = size;
+  var i = 0;
+  var newArray = [];
+  while(arr.length >= size){
+    arr1 = arr.slice(i,size);
+    i = i + foo;
+    size = size + foo;
+
+    newArray.push(arr1);
+  }
+  return newArray;
+}
+
+console.log(chunk(['a', 'b', 'c', 'd','e','f'], 4));
