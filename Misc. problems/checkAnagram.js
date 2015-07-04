fd6b7f99764d6ded96c9be0ed6bbc18a254d9df7
Sanchit Sharma <sanchitsharma1209@gmail.com>
2015-07-05 01:14:31 +0530

    Delete checkAnagram.js

--- Misc. problems/checkAnagram.js
+++
@@ -1,8 +0,0 @@
-function checkAnagram(str1, str2) {
-
-    if (str1.split("").sort().join(""); === str2.split("").sort().join("");)
-        console.log("yes, both strings are anagrams !!")
-
-    else
-        console.log("no, strings are not anagrams !!")
-}
