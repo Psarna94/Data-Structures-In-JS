7eb29c5b28427129227c31f708692aed075f22e2
Pranu Sarna <pranusarna@Pranus-MacBook-Air.local>
2015-08-17 23:28:56 +0530

    Radix sort completed

--- Queues Implementation/radix sort.js
+++ Queues Implementation/radix sort.js
@@ -1,4 +1,6 @@
 // construcot function for the Queue
+"use strict";
+
 function Queue(){
   this.dataStore = [];
   this.enqueue = enqueue;
@@ -47,3 +49,55 @@ function empty(){
     return false;
   }
 }
+
+// function to distribute numbers into bins based on their digits
+function distribute(nums, queues, n, digit){
+
+  for(var i=0; i<n; i++){
+    if(digit===1){
+      queues[nums[i]%10].enqueue(nums[i]);
+    }else{
+      queues[Math.floor(nums[i]/10)].enqueue(nums[i]);
+    }
+  }
+
+}
+
+// function to attain the list of numbers from the queues
+function collect(){
+  var i = 0;
+  for(var digit = 0; digit<10; digit++){
+    while(!queues[digit].empty){
+      nums[i++] = queues[digit].dequeue;
+    }
+  }
+}
+
+// function to display all the elements of an array
+function dispArray(arr){
+  arr.forEach(function(foo){
+    console.log(foo);
+  });
+}
+
+//generate queues or bins for each digit
+var queues = [];
+for(var i=0; i<10; i++){
+  queues[i] = new Queue();
+}
+
+// generate random numbers to perform radix sort on
+var nums = [];
+for(var i=0; i<10; i++){
+  nums[i] = Math.floor(Math.floor(Math.random()*101));
+}
+
+dispArray(nums);
+distribute(nums, queues, 10, 1);
+collect(queues, nums);
+console.log("after first sort");
+dispArray(nums);
+distribute(nums, queues, 10, 10);
+collect(queues, nums);
+console.log("after first sort");
+dispArray(nums);
