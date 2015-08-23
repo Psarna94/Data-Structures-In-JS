ae379e4743901e8473c719d0ec782ed244c09e03
Pranu Sarna <pranusarna@gmail.com>
2015-08-23 16:29:38 +0530

    Added the Sers class

---
+++ sets-implementation/sets.js
@@ -0,0 +1,10 @@
+function Set(){
+  this.dataStore = [];
+  this.add = add;
+  this.remove = remove;
+  this.union = union;
+  this.intersect = intersect;
+  this.subset = subset;
+  this.difference = difference;
+  this.show = show;
+}
