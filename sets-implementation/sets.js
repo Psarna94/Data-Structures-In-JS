ea5cc3c155a6c8e5d56b1397423f0decfd655d34
Pranu Sarna <pranusarna@gmail.com>
2015-08-23 16:36:09 +0530

    Added the add() and remove() function

--- sets-implementation/sets.js
+++ sets-implementation/sets.js
@@ -8,3 +8,24 @@ function Set(){
   this.difference = difference;
   this.show = show;
 }
+
+// a set can only store unique members therefore applying a check
+function add(data){
+  if(this.dataStore.indexOf(data)<0){
+    this.dataStore.push(data);
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+// similar to add function checking for index and then removing
+function remove(data){
+  if(this.dataStore.indexOf(data)<0){
+    return false;
+  }else{
+    this.dataStore.splice(this.dataStore.indexOf(data), 1);
+    return true
+  }
+};
