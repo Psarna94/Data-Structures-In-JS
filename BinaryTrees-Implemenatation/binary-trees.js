8da4f5b24e83a25de85999ee7875b5c6588cf593
Pranu Sarna <pranusarna@gmail.com>
2015-08-24 10:38:12 +0530

    Node and node show contructor added

---
+++ BinaryTrees-Implemenatation/binary-trees.js
@@ -0,0 +1,12 @@
+// Node Constructor for a binary tree
+function Node(data, left, right){
+  this.data = data;
+  this.left = left;
+  this.right = right;
+  this.show = show;
+};
+
+// function to show the Node
+function show(){
+  return this.data
+};
