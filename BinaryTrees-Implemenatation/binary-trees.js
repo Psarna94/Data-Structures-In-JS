63bc69e2fc26d75ddd07978644a1cb511c9b1ee2
Pranu Sarna <pranusarna@gmail.com>
2015-08-24 10:49:17 +0530

    Added the insert function of BST

--- BinaryTrees-Implemenatation/binary-trees.js
+++ BinaryTrees-Implemenatation/binary-trees.js
@@ -10,3 +10,37 @@ function Node(data, left, right){
 function show(){
   return this.data
 };
+
+// constructor class for the BSTs
+function BST(){
+  this.root  = null;
+  this.insert = insert;
+  // this.inOrder = inOrder;
+}
+
+// function to insert a node in the BST
+function insert(data){
+  var n = new Node(data, null, null);
+  if(this.root === null){
+    this.root = n;
+  }else{
+    var current = this.root;
+    var parent;
+    while(true){
+      parent = current;
+      if(data < current.data){
+        current = current.left;
+        if(current == null){
+          parent.left = n;
+          break;
+        }
+      }else{
+        current = current.right;
+        if(current = null){
+          parent.right = n;
+          break;
+        }
+      }
+    }
+  }
+};
