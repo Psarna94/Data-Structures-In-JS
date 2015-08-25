e481b45c919d5b25af6c6fd19defa04cb5c7c654
Pranu Sarna <pranusarna@gmail.com>
2015-08-25 13:39:05 +0530

    Updating Binary trees for Spidermonkey

--- BinaryTrees-Implemenatation/binary-trees.js
+++ BinaryTrees-Implemenatation/binary-trees.js
@@ -1,3 +1,4 @@
+
 // Node Constructor for a binary tree
 function Node(data, left, right){
   this.data = data;
@@ -15,32 +16,47 @@ function show(){
 function BST(){
   this.root  = null;
   this.insert = insert;
-  // this.inOrder = inOrder;
+  this.inOrder = inOrder;
 }
 
 // function to insert a node in the BST
-function insert(data){
+function insert(data) {
   var n = new Node(data, null, null);
-  if(this.root === null){
+  if (this.root == null) {
     this.root = n;
-  }else{
-    var current = this.root;
-    var parent;
-    while(true){
-      parent = current;
-      if(data < current.data){
-        current = current.left;
-        if(current == null){
-          parent.left = n;
-          break;
-        }
-      }else{
-        current = current.right;
-        if(current = null){
-          parent.right = n;
-          break;
-        }
-      }
-    }
   }
-};
+  else {
+    var current = this.root; var parent;
+    while (true) {
+      parent = current;
+      if (data < current.data) {
+        current = current.left;
+        if (current == null) {
+               parent.left = n;
+               break;
+              }
+            } else {
+              current = current.right; if (current == null) {
+               parent.right = n;
+break; }
+} }
+} }
+
+function inOrder (node){
+  if(!(node == null)){
+    inOrder(node.left);
+    print(node.show()+" ");
+    inOrder (node.right);
+  }
+}
+
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+print("Inorder traversal: ");
+inOrder(nums.root)
